cc0ab0433e5a76d5902c71a68db9b3705ecd2ceb
Guiso92 <emilioguisolfo@gmail.com>
2020-05-05 15:19:27 -0300

    Updates

---
+++ precache-manifest.9e3736832698318c6f072046548fc587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8299e29c3cb4f30b3d1d5aa774ad4e89",
+    "url": "/mernApp/index.html"
+  },
+  {
+    "revision": "9e74203dc5290eb51c6c",
+    "url": "/mernApp/static/css/main.97e5ec63.chunk.css"
+  },
+  {
+    "revision": "678e32fba30c9466f9a7",
+    "url": "/mernApp/static/js/2.aed76c14.chunk.js"
+  },
+  {
+    "revision": "9e74203dc5290eb51c6c",
+    "url": "/mernApp/static/js/main.38822e8a.chunk.js"
+  },
+  {
+    "revision": "e5cadc23895b2a935ddb",
+    "url": "/mernApp/static/js/runtime-main.8df0f7a3.js"
+  }
+]);
